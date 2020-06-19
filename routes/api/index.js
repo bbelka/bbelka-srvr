24a873e77d48ed7f59efc7c26ecba263ac107a9d
bbtwots <brettbelka@gmail.com>
2020-06-19 15:20:10 -0700

    url schema, controller, and routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
 const projectRoutes = require("./project");
+const urlRoutes = require("./url");
 
 //Project routes
 router.use("/project", projectRoutes);
+
+//Url routes
+router.use("/url", urlRoutes);
 
 module.exports = router;
---
+++ routes/api/url.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const urlsController = require("../../controllers/urlsController.js"); 
+
+//url routes: '/api/url'
+router.route("/")
+    // .get(urlsController.findAll)
+    .post(urlsController.create);
+
+//url rouges by ID: '/api/url/:id'
+router.route("/:id")
+    .get(urlsController.findById)
+    .put(urlsController.update)
+    .delete(urlsController.remove);
+
+
+module.exports = router;
