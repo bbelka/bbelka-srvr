3c5b10e0d94cce64b33eb6edad4f0313a7ffccaf
bbtwots <brettbelka@gmail.com>
2021-03-16 10:47:43 -0700

    setup create user, finalize env

--- models/User.js
+++ models/User.js
@@ -1,5 +1,12 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
+
+const hashPassword = (password) => {
+    let salt = bcrypt.genSaltSync();
+    let hash = bcrypt.hashSync(password, salt);
+    return hash;
+}
 
 const UserSchema = new Schema(
     {
@@ -7,6 +14,18 @@ const UserSchema = new Schema(
         password: String,
     }
 )
+
+UserSchema.methods.comparePassword = function (password) {
+    if (!this.password) { return false }
+    return bcrypt.compareSync(password, this.password);
+};
+
+UserSchema.pre('save', function (next) {
+    if (this.password && this.isModified('password')) {
+        this.password = hashPassword(this.password);
+    }
+    next();
+})
 
 const User = mongoose.model("User", UserSchema);
 
