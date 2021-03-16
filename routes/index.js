6f1feabf3d05af4ff2a6bd2ec1f6a45df8e1113e
bbtwots <brettbelka@gmail.com>
2021-03-16 10:47:43 -0700

    setup create user, finalize env

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,20 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const mailRoutes = require('./mailer');
+const authRoutes = require('./auth')
 
 //Mail route
 router.use("/mail", mailRoutes);
 
 //api route
 router.use("/api", apiRoutes);
+
+//auth route
+router.use("/auth", authRoutes);
+
+//readsessions
+router.get("/readsessions", (req,res)=>{
+    res.json(req.user)
+});
 
 module.exports = router;
